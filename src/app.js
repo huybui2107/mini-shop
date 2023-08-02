b686175df44f68bafb6209f4c23f7e8264abc9a1
noname21-stack <vanhuybuivips@gmail.com>
2023-08-02 22:11:42 +0700

    change URL database

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 const cookie_parser = require('cookie-parser');
 dotenv.config({ path: path.join(__dirname, '.env') });
 
-
+const URL_MONGODB ="mongodb+srv://huybui:pCS921fgH36dL24J@mini-shop.shc9tfw.mongodb.net/mini-shop"
 
 
 app.use(express.json());
@@ -34,10 +34,31 @@ app.use((err, req, res ,next) => {
 })
 
 
-mongoose.connect('mongodb://localhost:27017/mini_shop')
+mongoose.connect(URL_MONGODB)
     .then( () =>{
         console.log("Connected to MongoDB successfully")
         app.listen(process.env.PORT, () =>{
             console.log('Connecting to server ' );
         });
     })
+// const { MongoClient } = require("mongodb");
+// const username = encodeURIComponent("huybui");
+// const password = encodeURIComponent("Huy123@");
+// const cluster = "<clusterName>";
+// const authSource = "<authSource>";
+// const authMechanism = "<authMechanism>";
+// let uri =
+//   `mongodb+srv://${username}:${password}@${cluster}/?authSource=${authSource}&authMechanism=${authMechanism}`;
+// const client = new MongoClient(uri);
+// async function run() {
+//   try {
+//     await client.connect();
+//     const database = client.db("<dbName>");
+//     const ratings = database.collection("<collName>");
+//     const cursor = ratings.find();
+//     await cursor.forEach(doc => console.dir(doc));
+//   } finally {
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
